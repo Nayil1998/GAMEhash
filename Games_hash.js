7345eba08191c3ca54ad79da83229b5f4c1476d7
NAYIL ALHRBI <naelx13@gmail.com>
2025-11-27 22:43:42 +0300

    Update Games_hash.js

--- Games_hash.js
+++ Games_hash.js
@@ -1,12 +1,12 @@
 {
   "bounce": {
     "update": true,
-    "hash": "b4b91d025326eb10f25ad1f3030f7b35abca60d9",
+    "hash": "b4b91d025326eb10f25ad1f3030f7b35abca60d9b4b91d025326eb10f25ad1f3030f7b35abca60d9",
     "url": "https://raw.githubusercontent.com/mmvp0410-max/Bounce/refs/heads/main/main.js"
   },
   "pool": {
     "update": true,
-    "hash": "99",
+    "hash": "b103f6b46b79a9dfeac45401ef75734842253382",
     "url": "https://raw.githubusercontent.com/mmvp0410-max/pool/refs/heads/main/main.js"
   },
   "archery": {
