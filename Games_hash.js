c3c7164f970b36258fd1ee20ef8ed0172fd40575
NAYIL ALHRBI <naelx13@gmail.com>
2025-11-27 16:04:18 +0300

    Add Games_hash.js with game metadata

---
+++ Games_hash.js
@@ -0,0 +1,14 @@
+{
+  "bounce": {
+    "hash": "b4b91d025326eb10f25ad1f3030f7b35abca60d9",
+    "url": "https://raw.githubusercontent.com/mmvp0410-max/Bounce/refs/heads/main/main.js"
+  },
+  "pool": {
+    "hash": "b103f6b46b79a9dfeac45401ef75734842253382",
+    "url": "https://raw.githubusercontent.com/mmvp0410-max/pool/refs/heads/main/main.js"
+  },
+  "archery": {
+    "hash": "908dbde7ad8c4713dc565bd0e0f848a0ae4356c7",
+    "url": "https://raw.githubusercontent.com/mmvp0410-max/Archery/refs/heads/main/main.js"
+  }
+}
