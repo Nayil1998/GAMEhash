f57bfeff9946636bd635086a8f0a4d0c5d8f5414
NAYIL ALHRBI <naelx13@gmail.com>
2025-11-28 14:56:21 +0300

    Update Games_hash.js

--- Games_hash.js
+++ Games_hash.js
@@ -1,17 +1,43 @@
-{
-  "bounce": {
-    "update": true,
-    "hash": "b4b91d025326eb10f25ad1f3030f7b35abca60d9",
-    "url": "https://raw.githubusercontent.com/mmvp0410-max/Bounce/refs/heads/main/main.js"
-  },
-  "pool": {
-    "update": true,
-    "hash": "b103f6b46b79a9dfeac45401ef75734842253382",
-    "url": "https://raw.githubusercontent.com/mmvp0410-max/pool/refs/heads/main/main.js"
-  },
-  "archery": {
-    "update": true,
-    "hash": "908dbde7ad8c4713dc565bd0e0f848a0ae4356c7",
-    "url": "https://raw.githubusercontent.com/mmvp0410-max/Archery/refs/heads/main/main.js"
-  }
-}
+var GAME_MAP = {
+    bounce: {
+        hash: "b4b91d025326eb10f25ad1f3030f7b35abca60d9",
+        url: "https://raw.githubusercontent.com/mmvp0410-max/Bounce/refs/heads/main/main.js"
+    },
+    pool: {
+        hash: "b103f6b46b79a9dfeac45401ef75734842253382",
+        url: "https://raw.githubusercontent.com/mmvp0410-max/pool/refs/heads/main/main.js"
+    },
+    archery: {
+        hash: "908dbde7ad8c4713dc565bd0e0f848a0ae4356c7", 
+        url: "https://raw.githubusercontent.com/mmvp0410-max/Archery/refs/heads/main/main.js"
+    }
+};
+
+var POOL_HASH = "b103f6b46b79a9dfeac45401ef75734842253382";
+
+var POOL_IMPORTS = [
+    {
+        name: "rzbpyy747b3tp.json",
+        url: "https://raw.githubusercontent.com/mmvp0410-max/CUESTICKS/refs/heads/main/rzbpyy747b3tp.json"
+    },
+    {
+        name: "blo671p9f2jnp.json",
+        url: "https://raw.githubusercontent.com/mmvp0410-max/CUESTICKS/refs/heads/main/blo671p9f2jnp.json"
+    },
+    {
+        name: "6o81us8qsfp99.json",
+        url: "https://raw.githubusercontent.com/mmvp0410-max/THEMES/refs/heads/main/6o81us8qsfp99.json"
+    },
+    {
+        name: "4t51jpuzs2fz9.json",
+        url: "https://raw.githubusercontent.com/mmvp0410-max/THEMES/refs/heads/main/4t51jpuzs2fz9.json"
+    },
+    {
+        name: "nuyzp389zbtmy.json",
+        url: "https://github.com/mmvp0410-max/SPECIALFX/raw/refs/heads/main/nuyzp389zbtmy.json"
+    },
+    {
+        name: "ym1yo9sqm2zuz.json",
+        url: "https://github.com/mmvp0410-max/SPECIALFX/raw/refs/heads/main/ym1yo9sqm2zuz.json"
+    }
+];
